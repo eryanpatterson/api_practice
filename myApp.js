f83d33b94b1430fc59bfc0bdf0ba0c3bb5b65d25
ethan.patterson <eryanpatterson@gmail.com>
2021-07-15 15:21:31 -0400

    testing

--- myApp.js
+++ myApp.js
@@ -7,10 +7,10 @@ app.use('/public', express.static(public));
 
 const index = __dirname + '/views/index.html';
 
-app.get('/', function(req, res) {
+/* app.get('/', function(req, res) {
     res.sendFile(index);
 });
-
+*/
 
 
 
