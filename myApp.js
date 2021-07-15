9bd6e0d5cc4f8391827a4f164842b084a1133063
ethan.patterson <eryanpatterson@gmail.com>
2021-07-15 14:39:52 -0400

    fix

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var app = express();
 
 console.log("Hello World");
 
-const index = __dirname + /views/index.html;
+const index = __dirname + '/views/index.html';
 
 app.get('/', res.sendFile(index));
 
