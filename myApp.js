1f116eb78c4d1a7286377e3d1266de3d4c0da7af
ethan.patterson <eryanpatterson@gmail.com>
2021-07-16 12:39:36 -0400

    changed db uri

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://codie:uecAkJG81tkiruuJ@cluster0.h6lbc.mongodb.net/exambulance-test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
 let Person;
 
